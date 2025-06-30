9bcf4eb972da78dad98b6f25e8c4d016a512550d
Kareem Wahby <kimowahby@gmail.com>
2025-07-01 02:00:20 +0300

    responsive homepage + scroll

--- logic.js
+++ logic.js
@@ -4,12 +4,42 @@ const navEducation = document.getElementById("education");
 const navExperience = document.getElementById("experience");
 const navProjects = document.getElementById("projects");
 const navContacts = document.getElementById("contact");
+const pageHome = document.getElementById("homePage");
+const pageAbout = document.getElementById("aboutPage");
+const pageEducation = document.getElementById("educationPage");
+const pageExperience = document.getElementById("experiencePage");
+const pageProjects = document.getElementById("projectsPage");
+const pageContacts = document.getElementById("contactPage");
 
 let navList = [navHome, navAbout, navEducation, navExperience, navProjects, navContacts];
+let pages = [pageHome, pageAbout, pageEducation, pageExperience, pageProjects, pageContacts];
+
+let scrollEnabled = true;
 navList.forEach(nav =>{
     nav.addEventListener("click",function (){
+        scrollEnabled = false;
         navList.forEach(n=> n.classList.replace("active","inactive"))
         this.classList.remove("inactive");
         this.classList.add("active")
+
+        setTimeout(() => {
+            scrollEnabled = true;
+        },500);
+    });
+});
+
+window.addEventListener("scroll", function(){
+    if (!scrollEnabled) return;
+
+    pages.forEach((page,index) =>{
+        let position = page.getBoundingClientRect();
+        if (position.top <= 80 && position.bottom >= 80){
+            navList.forEach(s => {
+                s.classList.remove("active",)
+                s.classList.add("inactive")
+            });
+            navList[index].classList.remove("inactive");
+            navList[index].classList.add("active");
+        }
     });
 });
